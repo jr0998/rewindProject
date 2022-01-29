059648af9ccf25f089e6f0ded4db9a0cf5a54173
jr0998 <33519065+jr0998@users.noreply.github.com>
2022-01-29 12:44:30 -0500

    first js commit

--- js/site.js
+++ js/site.js
@@ -1,4 +1,25 @@
-//call hello world
-function helloWorld(){
-    alert("Hello World");
+//get String From document
+function getString() {
+    document.getElementById("alert").classList.add("invisible");
+    let strInput = document.getElementById("stringInput").value;
+    let reverseStr;
+
+    reverseStr = reverseString(strInput);
+
+    displayString(reverseStr);
+}
+
+//Reverse the String
+function reverseString(str) {
+    let reverseStr = [];
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr += str[i];
+    }
+    return reverseStr;
+}
+//Display String
+function displayString(str) {
+    document.getElementById("msg").innerHTML = str;
+    document.getElementById("alert").classList.add("visible");
 }
